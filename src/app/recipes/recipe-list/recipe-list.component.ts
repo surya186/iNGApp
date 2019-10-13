c8d3192ad0e80243e3e1cf6dbc4dddd9dc99cde9
surya186 <kirantripurari9@gmail.com>
2019-10-13 21:08:01 +0530

    Displaying recipe list Note: unrelated minor changes in the other two files

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -9,7 +9,9 @@ import { Recipe } from '../recipe.model';
 export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] =[
-    new Recipe('Test Recipe', 'Test Recipe description', 'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Cremajpg.jpg')
+    new Recipe('Test Recipe', 'Test Recipe description', 'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Cremajpg.jpg'),
+    new Recipe('Test 2 Recipe', 'Test 2 Recipe description', 'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Cremajpg.jpg'),
+    new Recipe('Test 3 Recipe', 'Test 3 Recipe description', 'https://pinchofyum.com/wp-content/uploads/Buffalo-Cauliflower-Tacos-with-Avocado-Cremajpg.jpg')
   ];
 
   constructor() { }
